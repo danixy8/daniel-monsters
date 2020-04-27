7a3b6be9f3f5a40cded6b73ce41ad52fe6320789
Daniel <danielguve2@gmail.com>
2020-04-26 22:37:57 -0400

    Updates

---
+++ precache-manifest.c6954dca766d19b9c6e8cbc981834659.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17f52f1a958fbda3cf0c1801444f72dd",
+    "url": "/daniel-monsters/index.html"
+  },
+  {
+    "revision": "6b58e925ad1a07392471",
+    "url": "/daniel-monsters/static/css/main.4d90395c.chunk.css"
+  },
+  {
+    "revision": "5d4c108c7a97ebb9d356",
+    "url": "/daniel-monsters/static/js/2.e7b871d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/daniel-monsters/static/js/2.e7b871d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b58e925ad1a07392471",
+    "url": "/daniel-monsters/static/js/main.a95f6f64.chunk.js"
+  },
+  {
+    "revision": "88c846c1a272e1b725b7",
+    "url": "/daniel-monsters/static/js/runtime-main.cf7a98b4.js"
+  }
+]);
